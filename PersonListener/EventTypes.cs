c42fd547da4a00f5dcaba901dcf5b5ebabab4c0e
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-08-27 17:48:38 +0100

    dotnet format

--- PersonListener/EventTypes.cs
+++ PersonListener/EventTypes.cs
@@ -3,7 +3,7 @@ namespace PersonListener
     public static class EventTypes
     {
         // Define the event types this service will be interested in here.
-        public const string TenureCreatedEvent = "TenureCreatedEvent"; 
+        public const string TenureCreatedEvent = "TenureCreatedEvent";
         public const string PersonAddedToTenureEvent = "PersonAddedToTenureEvent";
     }
 }
